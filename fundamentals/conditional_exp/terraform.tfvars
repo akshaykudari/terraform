26074288823dda1dd5c618d7f6b1e518584593a0
Akshay K <kudariakshay0@gmail.com>
2026-03-03 02:56:33 +0530

    added conditional expression files

---
+++ fundamentals/conditional_exp/terraform.tfvars
@@ -0,0 +1,6 @@
+region      = "ap-south-1"
+env         = "dev"
+key         = "key"
+ami         = "ami-019715e0d74f695be"
+volume_size = 10
+volume_type = "gp3"
---
+++ fundamentals/conditional_exp/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type = string
+}
+variable "env" {
+  type = string
+}
+
+variable "key" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+variable "volume_type" {
+  type = string
+}
+variable "volume_size" {
+  type = number
+}
